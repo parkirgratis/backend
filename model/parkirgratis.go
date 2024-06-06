f45d54a4365590ed8b048fbafa4ea52623f5c51d
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-06-06 16:52:17 +0700

    Backend ||| menambahkan struck

---
+++ model/parkirgratis.go
@@ -0,0 +1,16 @@
+package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Tempat struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Nama_Tempat string             `bson:"nama_tempat,omitempty" json:"nama_tempat,omitempty"`
+	Lokasi      string             `bson:"lokasi,omitempty" json:"lokasi,omitempty"`
+	Fasilitas   string             `bson:"fasilitas,omitempty" json:"fasilitas,omitempty"`
+	Lon         float64            `bson:"lon,omitempty" json:"lon,omitempty"`
+	Lat         float64            `bson:"lat,omitempty" json:"lat,omitempty"`
+}
+
+type Koordinat struct {
+	Markers [][]float64 `json:"markers"`
+}

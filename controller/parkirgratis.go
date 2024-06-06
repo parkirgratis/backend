f7231150e827a9e409793e6462dfcce5959cafe2
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-06-06 17:03:14 +0700

    Backend ||| menambahkan fungsi getlokasi

---
+++ controller/parkirgratis.go
@@ -0,0 +1,22 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gocroot/config"
+	"github.com/gocroot/helper"
+	"github.com/gocroot/helper/atdb"
+	"github.com/gocroot/model"
+	"github.com/whatsauth/itmodel"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func GetLokasi(respw http.ResponseWriter, req *http.Request) {
+	var resp itmodel.Response
+	kor, err := atdb.GetAllDoc[[]model.Tempat](config.Mongoconn, "tempat", bson.M{})
+	if err != nil {
+		resp.Response = err.Error()
+		helper.WriteResponse(respw, http.StatusBadRequest, resp)
+	}
+	helper.WriteResponse(respw, http.StatusOK, kor)
+}

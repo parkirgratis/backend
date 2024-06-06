c14483dd54ac84594f7ae44d1f5f5fb438eb9e12
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-06-06 17:41:04 +0700

    Backend ||| menambahkan struck gambar

--- model/parkirgratis.go
+++ model/parkirgratis.go
@@ -9,6 +9,7 @@ type Tempat struct {
 	Fasilitas   string             `bson:"fasilitas,omitempty" json:"fasilitas,omitempty"`
 	Lon         float64            `bson:"lon,omitempty" json:"lon,omitempty"`
 	Lat         float64            `bson:"lat,omitempty" json:"lat,omitempty"`
+	Gambar      string            `bson:"gambar,omitempty" json:"gambar,omitempty"`
 }
 
 type Koordinat struct {

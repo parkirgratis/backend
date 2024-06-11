e64eacd0d838a3dfa543d6c180ccbb110716abd7
Revan232 <revanyuda001@gmail.com>
2024-06-11 19:02:09 +0700

    Backend ||| memperbaiki post

--- controller/parkirgratis.go
+++ controller/parkirgratis.go
@@ -38,17 +38,7 @@ func GetMarker(respw http.ResponseWriter, req *http.Request) {
 	helper.WriteJSON(respw, http.StatusOK, mar)
 }
 
-func InsertTempat(db *mongo.Database, col string, namaTempat string, lokasi string, fasilitas string, lon float64, lat float64, gambar string) (insertedID primitive.ObjectID, err error) {
-	// Membuat map dari data tempat dengan kunci dan nilai yang sesuai
-	tempat := bson.M{
-		"nama_tempat": namaTempat, // Nama tempat
-		"lokasi":      lokasi,     // Lokasi tempat
-		"fasilitas":   fasilitas,  // Fasilitas yang tersedia
-		"lon":         lon,        // Longitude tempat
-		"lat":         lat,        // Latitude tempat
-		"gambar":      gambar,     // URL gambar tempat
-	}
-
+func InsertTempat(db *mongo.Database, col string, tempat model.Tempat) (insertedID primitive.ObjectID, err error) {
 	// Menyisipkan data tempat ke dalam koleksi MongoDB yang ditentukan
 	result, err := db.Collection(col).InsertOne(context.Background(), tempat)
 	if err != nil {
@@ -61,12 +51,8 @@ func InsertTempat(db *mongo.Database, col string, namaTempat string, lokasi stri
 	insertedID = result.InsertedID.(primitive.ObjectID)
 	return insertedID, nil // Mengembalikan ID yang disisipkan dan error nil (tidak ada error)
 }
-func InsertKoordinat(db *mongo.Database, col string, markers [][]float64) (insertedID primitive.ObjectID, err error) {
-	// Membuat map dari data koordinat dengan kunci dan nilai yang sesuai
-	koordinat := bson.M{
-		"markers": markers, // Array dari koordinat
-	}
 
+func InsertKoordinat(db *mongo.Database, col string, koordinat model.Koordinat) (insertedID primitive.ObjectID, err error) {
 	// Menyisipkan data koordinat ke dalam koleksi MongoDB yang ditentukan
 	result, err := db.Collection(col).InsertOne(context.Background(), koordinat)
 	if err != nil {
@@ -83,14 +69,7 @@ func InsertKoordinat(db *mongo.Database, col string, markers [][]float64) (inser
 // PostTempatParkir adalah fungsi yang menangani permintaan POST untuk menyimpan data tempat parkir baru.
 func PostTempatParkir(respw http.ResponseWriter, req *http.Request) {
 	// Membaca data dari body permintaan
-	var data struct {
-		NamaTempat string  `json:"nama_tempat"`
-		Lokasi     string  `json:"lokasi"`
-		Fasilitas  string  `json:"fasilitas"`
-		Lon        float64 `json:"lon"`
-		Lat        float64 `json:"lat"`
-		Gambar     string  `json:"gambar"`
-	}
+	var data model.Tempat
 	err := json.NewDecoder(req.Body).Decode(&data)
 	if err != nil {
 		// Jika terjadi kesalahan dalam mendekode data, kirimkan pesan kesalahan
@@ -99,7 +78,7 @@ func PostTempatParkir(respw http.ResponseWriter, req *http.Request) {
 	}
 
 	// Memanggil fungsi InsertTempat untuk menyisipkan data ke dalam database
-	insertedID, err := InsertTempat(config.Mongoconn, "tempat_parkir", data.NamaTempat, data.Lokasi, data.Fasilitas, data.Lon, data.Lat, data.Gambar)
+	insertedID, err := InsertTempat(config.Mongoconn, "tempat_parkir", data)
 	if err != nil {
 		// Jika terjadi kesalahan saat menyisipkan data, kirimkan pesan kesalahan
 		helper.WriteJSON(respw, http.StatusInternalServerError, itmodel.Response{Response: err.Error()})
@@ -113,9 +92,7 @@ func PostTempatParkir(respw http.ResponseWriter, req *http.Request) {
 // PostKoordinat adalah fungsi yang menangani permintaan POST untuk menyimpan data koordinat baru.
 func PostKoordinat(respw http.ResponseWriter, req *http.Request) {
 	// Membaca data dari body permintaan
-	var data struct {
-		Markers [][]float64 `json:"markers"` // Array dari koordinat
-	}
+	var data model.Koordinat
 	err := json.NewDecoder(req.Body).Decode(&data)
 	if err != nil {
 		// Jika terjadi kesalahan dalam mendekode data, kirimkan pesan kesalahan
@@ -124,7 +101,7 @@ func PostKoordinat(respw http.ResponseWriter, req *http.Request) {
 	}
 
 	// Memanggil fungsi InsertKoordinat untuk menyisipkan data ke dalam database
-	insertedID, err := InsertKoordinat(config.Mongoconn, "koordinat", data.Markers)
+	insertedID, err := InsertKoordinat(config.Mongoconn, "koordinat", data)
 	if err != nil {
 		// Jika terjadi kesalahan saat menyisipkan data, kirimkan pesan kesalahan
 		helper.WriteJSON(respw, http.StatusInternalServerError, itmodel.Response{Response: err.Error()})

7e0fea963fcbc0210ca610c18e70bfb629e90f6a
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-06-24 16:02:15 +0700

    Backend ||| memperbaiki cors

--- config/cors.go
+++ config/cors.go
@@ -33,7 +33,7 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	if r.Method == http.MethodOptions {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
-		w.Header().Set("Access-Control-Allow-Methods", "POST,GET")
+		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
 		w.Header().Set("Access-Control-Allow-Origin", "https://parkirgratis.github.io")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)

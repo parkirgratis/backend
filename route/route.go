946dad889ed576681f0bdce478d6eb437cc538d5
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-07-03 21:06:12 +0700

    add route

--- route/route.go
+++ route/route.go
@@ -32,6 +32,8 @@ func URL(w http.ResponseWriter, r *http.Request) {
 		controller.GetGithubFiles(w, r)
 	case method == "POST" && helper.URLParam(path, "/upload/:path"):
 		controller.PostUploadGithub(w, r)
+	case method == "PUT" && helper.URLParam(path, "/file/:path"):
+		controller.UpdateGithubFile(w, r)
 	case method == "PUT" && path == "/data/tempat":
 		controller.PutTempatParkir(w, r)
 	case method == "DELETE" && path == "/data/tempat":

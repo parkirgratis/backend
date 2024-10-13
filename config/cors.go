cf4296aef3bb8e9a6175dff44a2ec057a2848726
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-10-13 15:27:34 +0700

    fix cors block

--- config/cors.go
+++ config/cors.go
@@ -8,6 +8,7 @@ import (
 var AllowedOrigins = []string{
 	"https://geographicinformationsystem.github.io/parkirgratis/",
 	"https://parkirgratis.github.io.id",
+	"https://geographicinformationsystem.github.io"
 	"https://geographicinformationsystem.github.io/input/",
 	"http://127.0.0.1:5500",
 	"http://127.0.0.1:5501",

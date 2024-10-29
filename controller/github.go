6ca768b48d02b295d3969ab0ae47349242f1a084
rayfanaqbil <rayfana09@gmail.com>
2024-10-29 11:55:02 +0700

    add func PutUploadGithub

--- controller/github.go
+++ controller/github.go
@@ -64,3 +64,57 @@ func PostUploadGithub(w http.ResponseWriter, r *http.Request) {
 	helper.WriteJSON(w, http.StatusOK, respn)
 	fmt.Println("File upload process completed successfully")
 }
+
+func PutUpdateGithub(w http.ResponseWriter, r *http.Request) {
+	var respn itmodel.Response
+
+	fmt.Println("Starting file update process")
+
+	
+	_, header, err := r.FormFile("img")
+	if err != nil {
+		fmt.Println("Error parsing form file:", err)
+		respn.Response = err.Error()
+		helper.WriteJSON(w, http.StatusBadRequest, respn)
+		return
+	}
+
+	folder := helper.GetParam(r)
+	var pathFile string
+	if folder != "" {
+		pathFile = folder + "/" + header.Filename
+	} else {
+		pathFile = header.Filename
+	}
+
+
+	gh, err := atdb.GetOneDoc[model.Ghcreates](config.Mongoconn, "github", bson.M{})
+	if err != nil {
+		fmt.Println("Error fetching GitHub credentials:", err)
+		respn.Info = helper.GetSecretFromHeader(r)
+		respn.Response = err.Error()
+		helper.WriteJSON(w, http.StatusConflict, respn)
+		return
+	}
+
+	content, _, err := ghupload.GithubUpload(gh.GitHubAccessToken, gh.GitHubAuthorName, gh.GitHubAuthorEmail, header, "parkirgratis", "filegambar", pathFile, true)
+	if err != nil {
+		fmt.Println("Error updating file on GitHub:", err)
+		respn.Info = "gagal update github"
+		respn.Response = err.Error()
+		helper.WriteJSON(w, http.StatusEarlyHints, respn)
+		return
+	}
+	if content == nil || content.Content == nil {
+		fmt.Println("Error: content or content.Content is nil")
+		respn.Response = "Error updating file"
+		helper.WriteJSON(w, http.StatusInternalServerError, respn)
+		return
+	}
+
+	respn.Info = *content.Content.Name
+	respn.Response = *content.Content.Path
+	helper.WriteJSON(w, http.StatusOK, respn)
+	fmt.Println("File update process completed successfully")
+}
+

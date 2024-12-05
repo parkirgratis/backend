583d9dbfdd69dd8226156069d7c5014f6afac01c
rayfanaqbil <rayfana09@gmail.com>
2024-12-05 21:51:19 +0700

    Fix error region struct

--- model/gis.go
+++ model/gis.go
@@ -3,11 +3,11 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Region struct {
-	ID          primitive.ObjectID 	`bson:"_id,omitempty" json:"_id,omitempty"`
-	Longitude float64 				`bson:"long,omitempty" json:"long,omitempty"`
-	Latitude  float64 				`bson:"latomitempty" json:"lat,omitempty"`
-	Province    string             	`bson:"province,omitempty" json:"province,omitempty"`
-	District    string             	`bson:"district,omitempty" json:"district,omitempty"`
-	SubDistrict string             	`bson:"sub_district,omitempty" json:"sub_district,omitempty"`
-	Village     string             	`bson:"village,omitempty" json:"village,omitempty"`
+    ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+    Longitude   float64            `bson:"long,omitempty" json:"long,omitempty"`
+    Latitude    float64            `bson:"lat,omitempty" json:"lat,omitempty"`
+    Province    string             `bson:"province,omitempty" json:"province,omitempty"`
+    District    string             `bson:"district,omitempty" json:"district,omitempty"`
+    SubDistrict string             `bson:"sub_district,omitempty" json:"sub_district,omitempty"`
+    Village     string             `bson:"village,omitempty" json:"village,omitempty"`
 }
